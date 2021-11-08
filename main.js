02e768b7e8d7829983aac0b7ecd08cc112504598
Doug Hammer <doug.hammer16@gmail.com>
2021-11-08 14:53:43 -0800

    took out round gameplay, finished project

--- main.js
+++ main.js
@@ -2,11 +2,10 @@ const playerCreator = (player, sign, color, turn) => {
 	return { player, sign, color, turn };
 };
 
-const player1 = playerCreator('Player1', 'X',  '#eb4734', true);
+const player1 = playerCreator('Player1', 'X', '#eb4734', true);
 const player2 = playerCreator('Player2', 'O', '#202473', false);
 
 let turns = 9;
-
 let winner = null;
 
 const winningCombos = [
@@ -23,34 +22,36 @@ const winningCombos = [
 	[2, 4, 6],
 ];
 
-// gameBoard Object (holds player moves and renders moves to DOM)
 const gameBoard = (() => {
 	let _gameboard = [];
 
 	const checkForWinner = () => {
 		turns--;
-		let result = winningCombos.some((indices) => {
+		let result = winningCombos.find((indices) => {
 			if (
 				_gameboard[indices[0]] === player1.sign &&
 				_gameboard[indices[1]] === player1.sign &&
 				_gameboard[indices[2]] === player1.sign
 			) {
 				winner = player1;
+				displayController.displayWinner();
+
 			} else if (
 				_gameboard[indices[0]] === player2.sign &&
 				_gameboard[indices[1]] === player2.sign &&
 				_gameboard[indices[2]] === player2.sign
 			) {
 				winner = player2;
+				displayController.displayWinner();
+
 			} else if (turns === 0 && winner === null) {
-				console.log('TIE! NO WINNER')
+				displayController.displayWinner();
 			}
 		});
-		console.log(turns);
-		console.log(winner);
 		return result;
 	};
 
+	// Renders player moves to DOM and checks for winning combo
 	const playerTurn = (() => {
 		let cells = document.querySelectorAll('.cell');
 		cells.forEach((cell) => {
@@ -69,9 +70,7 @@ const gameBoard = (() => {
 						player2.turn = false;
 						player1.turn = true;
 						_gameboard[square.dataset.cellIndex] = player2.sign;
-					} 
-				} else if(turns = 0) {
-					console.log('TIE!')
+					}
 				}
 				checkForWinner();
 			});
@@ -79,40 +78,57 @@ const gameBoard = (() => {
 		return { cells };
 	})();
 
-	const resetGame = () => {
+	const resetBoard = () => {
 		_gameboard = [];
 		player1.turn = true;
 		player2.turn = false;
-
-		winner = null;
 		turns = 9;
-		console.clear();
+		winner = null;
+
+		document.querySelectorAll('.cell').forEach((cell) => {
+			cell.textContent = '';
+		});
 	};
 
-	return { playerTurn, checkForWinner, resetGame };
+	return { playerTurn, checkForWinner, resetBoard };
 })();
 
 const displayController = (() => {
 	const gameContainer = document.querySelector('.game-container');
 	const startArrows = document.querySelector('.start-arrows');
+	const title = document.querySelector('h1');
+	let winnerDisplay = document.querySelector('.winner-display');
 
 	const playGame = () => {
-		gameContainer.classList.remove('fadeOut');
 		startArrows.classList.add('fadeOut');
 		playBtn.classList.add('fadeOut');
 		resetBtn.classList.remove('fadeOut');
-	}
+		gameContainer.classList.remove('fadeOut');
+	};
 
-	const clearBoard = () => {
-		gameBoard.resetGame();
-		gameContainer.classList.add('fadeOut');
+	const displayWinner = () => {
+		if (winner) {
+			winnerDisplay.textContent = `Winner : ${winner.player} !`;
+			winnerDisplay.style.color = winner.color;
+			title.classList.add('fadeOut');
+			winnerDisplay.classList.remove('fadeOut');
+
+		} else {
+			winnerDisplay.textContent = `It's a Tie !`;
+			winnerDisplay.style.color = 'black';
+			title.classList.add('fadeOut');
+			winnerDisplay.classList.remove('fadeOut');
+		}
+	};
+
+	const resetGame = () => {
+		gameBoard.resetBoard();
+		title.classList.remove('fadeOut');
 		startArrows.classList.remove('fadeOut');
 		playBtn.classList.remove('fadeOut');
 		resetBtn.classList.add('fadeOut');
-	
-		document.querySelectorAll('.cell').forEach((cell) => {
-			cell.textContent = '';
-		});
+		gameContainer.classList.add('fadeOut');
+		winnerDisplay.classList.add('fadeOut');
 	};
 
 	// Event Listeners
@@ -120,7 +136,7 @@ const displayController = (() => {
 	playBtn.addEventListener('click', playGame);
 
 	const resetBtn = document.querySelector('.reset-btn');
-	resetBtn.addEventListener('click', clearBoard);
+	resetBtn.addEventListener('click', resetGame);
 
-	return { playGame, clearBoard };
+	return { playGame, displayWinner, resetGame };
 })();
